54fc7f6125345e3c2b3d31aa01ba3d56fe4cdb46
root <kvlad7492@gmail.com>
2021-08-08 04:34:22 +1000

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'z2m adapter settings': {
+        'en': 'Adapter settings for z2m',
+        'de': 'Adaptereinstellungen für z2m',
+        'ru': 'Настройки адаптера для z2m',
+        'pt': 'Configurações do adaptador para z2m',
+        'nl': 'Adapterinstellingen voor z2m',
+        'fr': "Paramètres d'adaptateur pour z2m",
+        'it': "Impostazioni dell'adattatore per z2m",
+        'es': 'Ajustes del adaptador para z2m',
+        'pl': 'Ustawienia adaptera dla z2m',
+        'zh-cn': 'z2m的适配器设置'
+    },
+    'option1': {
+        'en': 'IP',
+        'de': 'Option 1',
+        'ru': 'IP',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'Port',
+        'de': 'Option 2',
+        'ru': 'Порт',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
---
+++ main.js
@@ -0,0 +1,307 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v1.26.3
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+const WebSocket = require('../ws');
+
+
+/**
+ * The adapter instance
+ * @type {ioBroker.Adapter}
+ */
+let adapter;
+var z2m_command;
+/**
+ * Starts the adapter instance
+ * @param {Partial<utils.AdapterOptions>} [options]
+ */
+function startAdapter(options) {
+    // Create the adapter and define its methods
+    return adapter = utils.adapter(Object.assign({}, options, {
+        name: 'z2m',
+
+        // The ready callback is called when databases are connected and adapter received configuration.
+        // start here!
+        ready: main, // Main method defined below for readability
+
+        // is called when adapter shuts down - callback has to be called under any circumstances!
+        unload: (callback) => {
+            try {
+                // Here you must clear all timeouts or intervals that may still be active
+                // clearTimeout(timeout1);
+                // clearTimeout(timeout2);
+                // ...
+                // clearInterval(interval1);
+
+                callback();
+            } catch (e) {
+                callback();
+            }
+        },
+
+        // If you need to react to object changes, uncomment the following method.
+        // You also need to subscribe to the objects with `adapter.subscribeObjects`, similar to `adapter.subscribeStates`.
+        // objectChange: (id, obj) => {
+        //     if (obj) {
+        //         // The object was changed
+        //         adapter.log.debug(`object ${id} changed: ${JSON.stringify(obj)}`);
+        //     } else {
+        //         // The object was deleted
+        //         adapter.log.debug(`object ${id} deleted`);
+        //     }
+        // },
+
+        // is called if a subscribed state changes
+        stateChange: (id, state) => {
+            if (state) {
+                // The state was changed
+                if(state.from !='system.adapter.'+adapter.name+'.'+adapter.instance){
+				
+				adapter.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+				adapter.log.info(JSON.stringify(state));
+				var aid=id.split('.')
+				try{
+					z2m_command(aid[3],aid[4],state.val+"")
+				}catch{}
+				}				
+            } else {
+                // The state was deleted
+                adapter.log.debug(`state ${id} deleted`);
+            }
+        },
+
+        // If you need to accept messages in your adapter, uncomment the following block.
+        // /**
+        //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+        //  * Using this method requires "common.message" property to be set to true in io-package.json
+        //  */
+        // message: (obj) => {
+        //     if (typeof obj === 'object' && obj.message) {
+        //         if (obj.command === 'send') {
+        //             // e.g. send email or pushover or whatever
+        //             adapter.log.debug('send command');
+
+        //             // Send response in callback if required
+        //             if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+        //         }
+        //     }
+        // },
+    }));
+}
+
+async function main() {
+var z2m
+var z2m_buf_Devices=[];
+adapter.setStateAsync('info.connection', false, true)
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // adapter.config:
+    adapter.log.debug('config option1: ' + adapter.config.option1);
+    adapter.log.debug('config option2: ' + adapter.config.option2);
+		var adress='ws://'+adapter.config.option1+":"+adapter.config.option2+"/api"
+		adapter.log.debug('z2m adress: ' + adress);
+				 function z2mWebSocket() {
+					 try{
+					  z2m = new WebSocket(adress);
+					  z2m.on('open', 	function open() {z2m.send('hello');});
+					  z2m.on('message', function incoming(data) {z2m_parse(data);});
+					  z2m.on('error', function error(data) {adapter.log.debug(data)}); 
+					  }catch(err){adapter.log.debug(err)}
+				   }
+				   function onOpen(evt) {z2m.send('hello');}
+				   function z2m_send(message) {z2m.send(message);}
+				   var checkWSz2m = setInterval(function(){
+					 //  z2m.clients.forEach(function each(client) {if (client == z2m && client.readyState != WebSocket.OPEN) {z2mWebSocket();}});	
+					   adapter.log.debug("*************************checkWSz2m*******************************")			  
+					  if(z2m.readyState!=1){adapter.setStateAsync('info.connection', false, true);z2mWebSocket();}else{ adapter.setStateAsync('info.connection', true, true);}
+					   
+					   }, 5000);
+
+		z2mWebSocket();
+
+		async function z2m_parse(data){	
+		//adapter.log.debug(data)
+			var data=JSON.parse(data)
+				switch (data.topic)
+				{
+					case "bridge/config":
+						break;
+
+					case "bridge/info":
+
+						break;
+
+					case "bridge/state":
+
+					   break;
+
+					case "bridge/devices":
+						{
+							z2m_buf_Devices=data;
+							mk_tree_Device(z2m_buf_Devices);
+						}
+						break;
+
+					case "bridge/groups":
+
+						break;
+
+					case "bridge/event":
+						break;
+
+					case "bridge/extensions":
+
+						break;
+
+					case "bridge/logging":
+
+						break;
+
+					case "bridge/response/networkmap":
+
+					   break;
+
+
+					case "bridge/response/touchlink/scan":
+
+						break;
+
+					case "bridge/response/touchlink/identify":
+
+						break;
+
+					case "bridge/response/touchlink/factory_reset":
+
+						break;
+
+
+					default:
+					var dv=data.topic.split("/")		
+					if( dv.length <2 ){
+					
+					adapter.log.debug("z2m|"+dv[0]+" : "+JSON.stringify(data.payload))
+		//			z2m|rozetka : {"current":0.18,"linkquality":220,"power":18,"state":null,"voltage":229.8}
+					
+					
+							  for (let [key, value] of Object.entries(data.payload)) {	
+									 if (value!=null){await adapter.setState('dev.'+dv[0]+'.'+key, value, true)}	
+					
+							  }			
+						
+					}
+						break;
+				}
+		}
+    // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+    adapter.subscribeStates('dev.*');
+
+    // examples for the checkPassword/checkGroup functions
+    adapter.checkPassword('admin', 'iobroker', (res) => {
+        adapter.log.debug('check user admin pw iobroker: ' + res);
+    });
+
+    adapter.checkGroup('admin', 'admin', (res) => {
+        adapter.log.debug('check group user admin group admin: ' + res);
+    });
+
+
+async function mk_tree_Device(data){
+adapter.log.debug(JSON.stringify(data));
+var devarr=[];
+devarr=await z2m_zesp_Device(devarr,data)
+adapter.log.debug(JSON.stringify(devarr));
+}
+
+async function z2m_zesp_Device(devar,data){
+					var z2m_Devices=[];
+					for(var i =1;i < (data.payload).length;i++){
+							adapter.log.debug("------------data.payload[i]----------")
+							adapter.log.debug(JSON.stringify(data.payload[i]))				 			
+							var rep={};
+							if(data.payload[i].definition !=null ){
+							var exp=data.payload[i].definition.exposes;
+
+							rep=await getFeatures(rep,exp)								
+							
+							async function getFeatures(arep,aexp){	
+									for(var z =0;z < aexp.length;z++){
+										if( aexp[z].hasOwnProperty('features')){
+											getFeatures(arep,aexp[z].features)										
+										}else{
+										 Object.assign(arep,JSON.parse(`{"z2m_`+aexp[z].name+`" : {"label": "`+aexp[z].name+`","val": "x","mat": "1","role": "","parsed": "","time": 0,"access":`+aexp[z].access+`}	}`))
+var atr_type;
+        switch (aexp[z].type)
+		{
+ //           case "binary":
+ //              atr_type='boolean'; 
+//				break;
+            case "numeric":
+               atr_type='number'; 
+				break;				
+				
+			default :
+			//string/number/boolean
+			atr_type='string';
+		}
+										
+//adapter.createStateAsync('dev',data.payload[i].friendly_name, aexp[z].name, 'atr_state', {name: 'test', role:'state', type: atr_type ,read:true,write: true})											
+await adapter.createStateAsync('dev',data.payload[i].friendly_name, aexp[z].name, {
+				name: aexp[z].name, 
+				type: atr_type ,	
+				role:'state', 
+				read:true,
+				write: true,
+
+        native: {},		
+	})	
+
+
+	
+										}
+									}
+								return arep	
+								}	
+
+								devar.push(							
+								  {
+									"Device": H2(data.payload[i].network_address),
+									"Name": 	data.payload[i].friendly_name,
+									"IEEE": 	data.payload[i].ieee_address,
+									"ModelId":	data.payload[i].model_id,
+									"Location": "z2m",
+									"EP": {},
+									"Report": rep,
+									"DevType": "Z2M",									
+									"definition": data.payload[i].definition
+								  }
+								)
+							}
+					}					
+				
+				return devar;	
+}
+
+
+		function H2(input, base) {return input.toString(16).padStart( 4, 0).toUpperCase();}
+		z2m_command=function(ieee,pl_name,val){
+			z2m_send('{"payload":{"'+pl_name+'":"'+val+'"},"topic":"'+ieee+'/set"}')
+			//console.log('{"payload":{"'+pl_name+'":"'+val+'"},"topic":"'+ieee+'/set"}')
+		} 
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export startAdapter in compact mode
+    module.exports = startAdapter;
+} else {
+    // otherwise start the instance directly
+    startAdapter();
+}
